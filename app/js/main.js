7e9510adaafbb15a287842e604b8cdad9ee22d9b
KanishVlad <130839651+KanishVlad@users.noreply.github.com>
2024-05-27 14:16:14 +0300

    create reviews section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
-$(function(){
- 
-})
+var swiper = new Swiper(".reviews-swiper", {
+  slidesPerView: 3,
+  navigation: {
+
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
